2cc2594c2dc74fcb6c975e07bf01e37a5c3d0563
Unknown <giorgostheo97@gmail.com>
2018-08-31 00:12:28 +0300

    Added argparse multiple argument support for las

--- las.py
+++ las.py
@@ -2,28 +2,36 @@
 
 ## Lick (the timestamp) And Send it --> LAS
 
-import sys
+import sys, os
 import configparser
 import paramiko 
 import argparse
 from setuptools import find_packages
 from lib import connections
 
+config_path = './configs'
 
 servers = configparser.ConfigParser()
-servers.read('servers.ini')
+servers.read(os.path.join(config_path,'servers.ini'))
 
+workspaces = configparser.ConfigParser()
+workspaces.read(os.path.join(config_path,'workspace.ini'))
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--server',dest='servers_name',nargs='?',choices=servers.sections(), help='Pick a server from the available ones')
 parser.add_argument('--sync', default=False, action="store_true", help='Sync')
-#parser.add_argument('--workspace' ,nargs='?',choices=servers.sections(), help='Pick a workspace from the available ones')
-args = parser.parse_args()
+parser.add_argument('--server',dest='server_name',nargs='?',choices=servers.sections(), help='Pick a server from the available ones')
+parser.add_argument('--workspace',dest='workspace_name' ,nargs='?', help='Pick a workspace from the available ones')
 
-print (vars(args))
-# server_dict = connections.get_servers('servers.ini', args.server)
 
-# cmd = ' '.join(sys.argv[1:])
-# if cmd == '': print('Shoooot yooo foookin mout and type foookin smthing m8')
+args, ukargs = parser.parse_known_args()
 
-# server_dict['connection'].exec_command(cmd.split())
+# print ('known',vars(args))
+# print ('unknown',ukargs)
+
+server_dict = connections.get_servers('servers.ini', args.server_name)
+
+cmd = ' '.join(ukargs)
+if cmd == '': print('Shoooot yooo foookin mout and type foookin smthing m8')
+
+server_dict['connection'].exec_command(cmd.split())
+print (cmd)
